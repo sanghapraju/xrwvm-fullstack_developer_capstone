6d86362278a37239d0bf4d204bf70f5a429afa97
sanghapraju <120468473+sanghapraju@users.noreply.github.com>
2025-02-07 12:42:40 +0100

    settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -26,10 +26,17 @@ SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  False
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "sanghapraju-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://sanghapraju-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
